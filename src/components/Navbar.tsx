3fcd4f9595e86344b3505ebf4b13b1292e6b6711
SatwikPanda <tony943838@gmail.com>
2024-05-26 11:20:29 +0530

    env and nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,34 @@
 import { LoginButton } from "./auth/login-button";
 import { Button } from "./ui/button";
 import  AuthButton  from "./elements/authButton"
+import Link from "next/link";
 
 const Navbar = () => {
     return (
         <header>
             <nav>
-                <AuthButton text="Sign in"/>
+                <div className="px-9 py-2 flex justify-between items-center">
+                    <div className="mx-2">
+                        <Link href="#" className="font-bold text-xl tracking-tighter">Doccon</Link>
+                    </div>
+                    <div className="flex w-[620px] justify-between font-semibold">
+                        <Link href="#">Home</Link>
+                        <Link href="#">App</Link>
+                        <Link href="#">Pricing</Link>
+                        <Link href="#">EMR</Link>
+                        <Link href="#">Press Room</Link>
+                        <Link href="#">Company</Link>
+                        <Link href="#">Blog</Link>
+                        <Link href="#">FAQ</Link>
+                    </div>
+                    <div className="mx-2">
+                    <LoginButton>
+                        <Button variant="secondary" className="lg">
+                            Sign in
+                        </Button>
+                    </LoginButton>
+                    </div>
+                </div>
             </nav>
         </header>
     );
