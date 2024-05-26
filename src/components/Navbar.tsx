d97357d2568de3376aab4c702889a960bf832519
SatwikPanda <tony943838@gmail.com>
2024-05-26 12:01:44 +0530

    Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,19 +11,19 @@ const Navbar = () => {
                     <div className="mx-2">
                         <Link href="#" className="font-bold text-xl tracking-tighter">Doccon</Link>
                     </div>
-                    <div className="flex w-[620px] justify-between font-semibold">
-                        <Link href="#">Home</Link>
-                        <Link href="#">App</Link>
-                        <Link href="#">Pricing</Link>
-                        <Link href="#">EMR</Link>
-                        <Link href="#">Press Room</Link>
-                        <Link href="#">Company</Link>
-                        <Link href="#">Blog</Link>
-                        <Link href="#">FAQ</Link>
+                    <div className="flex justify-between tracking-tight hidden lg:block">
+                        <Link href="#" className="hover:bg-slate-300 px-4 rounded-full transition duration-450 ease-in-out transform py-1">Home</Link>
+                        <Link href="#" className="hover:bg-slate-300 px-4 rounded-full transition duration-450 ease-in-out transform py-1">App</Link>
+                        <Link href="#" className="hover:bg-slate-300 px-4 rounded-full transition duration-450 ease-in-out transform py-1">Pricing</Link>
+                        <Link href="#" className="hover:bg-slate-300 px-4 rounded-full transition duration-450 ease-in-out transform py-1">EMR</Link>
+                        <Link href="#" className="hover:bg-slate-300 px-4 rounded-full transition duration-450 ease-in-out transform py-1">Press Room</Link>
+                        <Link href="#" className="hover:bg-slate-300 px-4 rounded-full transition duration-450 ease-in-out transform py-1">Company</Link>
+                        <Link href="#" className="hover:bg-slate-300 px-4 rounded-full transition duration-450 ease-in-out transform py-1">Blog</Link>
+                        <Link href="#" className="hover:bg-slate-300 px-4 rounded-full transition duration-450 ease-in-out transform py-1">FAQ</Link>
                     </div>
                     <div className="mx-2">
                     <LoginButton>
-                        <Button variant="secondary" className="lg">
+                        <Button variant="custom" className="font-semibold hidden lg:block">
                             Sign in
                         </Button>
                     </LoginButton>
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -18,6 +18,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        custom: "bg-gray-300 rounded-full hover:bg-gray-400 hover:scale-105 hover:-translate-y-1 transition"
       },
       size: {
         default: "h-10 px-4 py-2",
