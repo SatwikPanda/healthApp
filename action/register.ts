b00a2fcae16c120dcc3faf75315bb538e20c7a04
Arya bardhan das <dasarya2002@gmail.com>
2024-06-04 23:05:33 +0530

    sdede

--- action/register.ts
+++ action/register.ts
@@ -1,7 +1,38 @@
 "use server";
 
+import {db} from "@/lib/db"
 import * as z from "zod";
+import bcrypt from "bcrypt";
 
+import { RegisterSchema } from "../schema";
+import { getUserByEmail } from "../data/user";
 
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+const validatedFields = RegisterSchema.safeParse(values);
 
-// import {RegisterSchema} from "@/schemas"
+   if(!validatedFields.success){
+    return{ error: "Invalid fields!"};
+   }
+
+   const { email, password, name} = validatedFields.data;
+   const hashedPassword = await bcrypt.hash(password, 10);
+
+   const existingUser =  await getUserByEmail(email)
+
+   if (existingUser) {
+    return{
+        error: "Email already in use!"
+    }
+   }
+
+   await db.user.create({
+    data:{
+        name,
+        email,
+        password,
+    },
+});
+
+//    TODO: Send verification token email
+   return {success: "User created!"}
+};
